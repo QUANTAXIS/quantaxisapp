525390fe1eb4928fcca3da10c291f1b1db0d5b52
yutiansut <yutiansut@qq.com>
2018-01-25 01:37:22 +0800

    #update

--- pages/quantaxis/quantaxis.js
+++ pages/quantaxis/quantaxis.js
@@ -1,14 +1,67 @@
-const util = require('../../utils/util.js')
-
+const app = getApp()
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    logs: []
+    user:[],
+    welcometext:'FINANCIAL QUANTITATIVE FRAMEWORK'
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('quantaxis') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log('already ok')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
